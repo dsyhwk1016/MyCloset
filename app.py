31964d73d47c6886249f480df1a9e534264955b3
inmyblue <inmyblue0930@naver.com>
2022-02-11 19:07:03 +0900

    í2022_02_11 1906 register API add

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import hashlib
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.mycloset
 
 
 @app.route('/')
@@ -11,6 +16,47 @@ def home():
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
+
+@app.route('/register', methods=['POST', 'GET']) #회원가입시 호출되는 API
+def register():
+    if request.method == 'POST': # 회원가입 Form 이 Submit 되었을시 실행(POST방식)
+
+        user_id = request.form['user_id']
+        user_pw = request.form['user_pw']
+        user_name = request.form['user_name']
+
+        shapw = hashlib.sha256(user_pw.encode()).hexdigest() #패스워드 암호화
+
+        doc = {
+            'user_id' : user_id,
+            'user_pw' : shapw,
+            'user_name' : user_name
+        }
+
+        db.member.insert_one(doc) #member 컬렉션에 insert
+
+        return render_template('index.html') #회원가입 완료후 index화면으로 이동
+
+    elif request.method == 'GET': #ID와 닉네임 중복체크시 호출되는 API
+        if request.args.get('type') == 'id': #ID중복체크시 실행
+            user_id = request.args.get('user_id') #GET방식으로 넘어온 파라미터를 변수에 저장
+
+            chk = list(db.member.find({'user_id' : user_id} , {'_id' : False})) #member 컬렉션에서 동일한 ID가 있는지 검사
+
+            if(chk):
+                return jsonify({'msg' : '이미 가입된 ID입니다'})
+            else:
+                return jsonify({'msg' : '가입이 가능한 ID입니다'})
+
+        elif request.args.get('type') == 'name': #닉네임 중복체크시 실행
+            user_name = request.args.get('user_name') #GET방식으로 넘어온 파라미터를 변수에 저장
+
+            chk = list(db.member.find({'user_name' : user_name}, {'_id' : False})) #member 컬렉션에서 동일한 닉네임이 있는지 검사
+
+            if(chk):
+                return jsonify({'msg' : '이미 가입된 닉네임입니다'})
+            else:
+                return jsonify({'msg' : '가입이 가능한 닉네임입니다'})
 
 
 if __name__ == '__main__':
