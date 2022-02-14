eefb2a29ef1d9791de5a9132f06c11d80b631970
inmyblue <inmyblue0930@naver.com>
2022-02-14 16:32:15 +0900

    2022_02_14 login

--- app.py
+++ app.py
@@ -67,13 +67,14 @@ def login():
     db_user = list(db.member.find({'user_id' : input_user_id}, {'_id' : False}))
 
     if input_user_id != db_user[0]['user_id']: #입력한 ID가 DB에 있는지 확인
-        return render_template(url_for('login_page'), id_chk = False)
+        return jsonify({"id_chk" : False})
 
     if input_user_id == db_user[0]['user_id'] and shapw == db_user[0]['user_pw']: #아이디 / 비밀번호 체크
         session['user_id'] = input_user_id #user_id 세션에 아이디 정보 입력
-        return redirect(url_for('home'))
+        return jsonify({"msg" : "로그인 성공!"})
 
-    return redirect((url_for('login_page'))) #맞는게 없으면 다시 로그인페이지로 이동
+    else :
+        return jsonify({"pw_chk" : False})
 
 @app.route('/google_check') #구글 첫로그인시 자동으로 member 컬렉션에 ID와 Name 입력
 def google_chk():
