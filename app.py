71733c894e023238f0098126491d25c565d7a155
inmyblue <inmyblue0930@naver.com>
2022-02-15 19:05:34 +0900

    2022_02_15 naver_login add

--- app.py
+++ app.py
@@ -26,13 +26,11 @@ app.register_blueprint(blueprint,url_prefix="/login")
 app.register_blueprint(user_bp,url_prefix="/login")
 app.register_blueprint(find_clothes.closet)
 
+
 @app.route('/')
 def home():
     #로그인 상태에 따라 index 로딩시 상태변수 전달 / 로그인페이지 => 로그아웃으로 변경
     logged = False
-    if google.authorized:
-        logged = True
-
     if "user_id" in session:
         logged = True
     return render_template('index.html', logged = logged)
