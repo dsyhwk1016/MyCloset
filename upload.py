8922ed1e33fc917ca3710526b4742a4524b29de3
jann <jjung5459@gmail.com>
2022-02-16 16:24:44 +0900

    0216 _ upload.py 구현 -ing

--- upload.py
+++ upload.py
@@ -1,32 +1,71 @@
-from flask import Flask, render_template, redirect, request, flash
-from werkzeug import secure_filename  # 파일 업로드 위한 설정값
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from pymongo import MongoClient
 import os
+import urllib.request
 
-import urlib.request
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-import app
+# MongoDB Setup
+client = MongoClient('localhost', 27017)
+db = client.mycloset
 
-app.secret_key ="mycloset"
-
-UPLOAD_FOLDER = './static/uploads'
-app.config[UPLOAD_FOLDER] = UPLOAD_FOLDER
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-def allowed_file(file_name):
-    return '-' in filename and filename.rsplit('.', 1).lower() in ALLOWED_EXTENSIONS
 
-# 업로드 HTML 렌더링
-@app.route('/upload')
-def render_file():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# class Mycloset(db.client): 참조 flask_mongoengine
+#     file = db.StringField()
+#     style = db.StringField()
+#     season = db.StringField()
+#     kind = db.StringField()
+#     color = db.StringField()
+
+@app.route('/', methods=['GET'])
+def upload():
     return render_template('upload.html')
 
 
-# 파일 업로드 처리
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    # file = request.files['file']
+    # style = request.form['uniform_style']
+    # season = request.form['uniform_season']
+    # kind = request.form['uniform_kind']
+    # color = request.form['uniform_color']
+    # filename = secure_filename(file.filename)
+
     if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
         file = request.files['file']
-        # 저장할 경로 + 파일명
-        file.save('./static/uploads/' + secure_filename(file.filename))
-        return 'uploads 디렉토리 -> 파일 업로드 성공!'
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # doc = {
+        #     'file': file,
+        #     'style': style,
+        #     'season': season,
+        #     'kind': kind,
+        #     'color': color
+        # }
+        # db.mycloset.insert_one(doc)
+        # flash('File successfully uploaded ' + file.filename + ' to the database!')
+        # return redirect('/')
+        else:
+            flash('Only png, jpg, jpeg, gif')
+            return redirect(request.url)
+    return render_template('upload.html')
